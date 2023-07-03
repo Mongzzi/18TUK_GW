72c60058c8c488643f362e132e5e316e2ef2fb53
KIM_NA_HYEON <hjna0206@naver.com>
2023-07-03 17:19:02 +0900

    카메라 , 타이머 클래스 생성

--- Dev/Enemy Slasher/Enemy Slasher/stdafx.cpp
+++ Dev/Enemy Slasher/Enemy Slasher/stdafx.cpp
@@ -1,8 +1,9 @@
 // stdafx.cpp : 표준 포함 파일만 들어 있는 소스 파일입니다.
-// LabProject03-1.pch는 미리 컴파일된 헤더가 됩니다.
+// pch는 미리 컴파일된 헤더가 됩니다.
 // stdafx.obj에는 미리 컴파일된 형식 정보가 포함됩니다.
 
 #include "stdafx.h"
+#include "d3dx12.h"
 
 UINT gnRtvDescriptorIncrementSize = 0;
 UINT gnDsvDescriptorIncrementSize = 0;
