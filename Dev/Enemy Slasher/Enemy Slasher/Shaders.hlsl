27896cc02f95a790a9e124c3b1e166c191ecd582
Park Ki Jeoung <parkyj477@gmail.com>
2023-07-06 15:01:35 +0900

    Shader.hlsl 및 Scene 코드 작성
    register 0 - camera
    register 1 - gameobject

---
+++ Dev/Enemy Slasher/Enemy Slasher/Shaders.hlsl
@@ -0,0 +1,48 @@
+cbuffer cbCameraInfo : register(b0)
+{
+	matrix		gmtxView : packoffset(c0);
+	matrix		gmtxProjection : packoffset(c4);
+	float3		gvCameraPosition : packoffset(c8);
+};
+
+cbuffer cbGameObjectInfo : register(b1)
+{
+	matrix		gmtxGameObject : packoffset(c0);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+struct VS_STANDARD_INPUT
+{
+	float3 position : POSITION;
+};
+
+struct VS_STANDARD_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float3 positionW : POSITION;
+};
+
+VS_STANDARD_OUTPUT VSStandard(VS_STANDARD_INPUT input)
+{
+	VS_STANDARD_OUTPUT output;
+
+	output.positionW = (float3)mul(float4(input.position, 1.0f), gmtxGameObject);
+	output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+
+	return(output);
+}
+
+float4 PSStandard(VS_STANDARD_OUTPUT input) : SV_TARGET
+{
+	float4 cAlbedoColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cSpecularColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+	float4 cEmissionColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	float4 cColor = cAlbedoColor + cSpecularColor + cEmissionColor;
+
+	cColor = float4(1.0f, 0.0f, 0.0f, 1.0f);
+
+	return(cColor);
+}
