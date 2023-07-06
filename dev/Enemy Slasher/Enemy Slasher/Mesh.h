29058390bf576cc360c2ddf89bf24aced1634765
KIM_NA_HYEON <hjna0206@naver.com>
2023-07-06 15:00:40 +0900

    메쉬 클래스 생성 ( 육면체 )

---
+++ dev/Enemy Slasher/Enemy Slasher/Mesh.h
@@ -0,0 +1,48 @@
+class CMesh
+{
+public:
+	CMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual ~CMesh();
+
+private:
+	int m_nReferences = 0;
+
+public:
+	void AddRef() { m_nReferences++; }
+	void Release() { if (--m_nReferences <= 0) delete this; }
+	void ReleaseUploadBuffers();
+
+protected:
+	ID3D12Resource* m_pd3dVertexBuffer = NULL;
+	ID3D12Resource* m_pd3dVertexUploadBuffer = NULL;
+	D3D12_VERTEX_BUFFER_VIEW m_d3dVertexBufferView;
+	
+	D3D12_PRIMITIVE_TOPOLOGY m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	
+	UINT m_nSlot = 0;
+	UINT m_nVertices = 0;
+	UINT m_nStride = 0;
+	UINT m_nOffset = 0;
+
+protected:
+	ID3D12Resource* m_pd3dIndexBuffer = NULL;
+	ID3D12Resource* m_pd3dIndexUploadBuffer = NULL;
+	D3D12_INDEX_BUFFER_VIEW m_d3dIndexBufferView;
+
+	UINT m_nIndices = 0;	// 인덱스 버퍼에 포함되는 인덱스의 개수
+	UINT m_nStartIndex = 0; // 인덱스 버퍼에서 메쉬를 그리기 위해 사용되는 시작 인덱스
+	int m_nBaseVertex = 0;  // 인덱스 버퍼의 인덱스에 더해질 인덱스
+
+public:
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList);
+
+};
+
+
+class CBoxMesh : public CMesh
+{
+
+public:
+	CBoxMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, float width = 2.0f, float height = 2.0f, float depth = 2.0f);
+	virtual ~CBoxMesh();
+};
