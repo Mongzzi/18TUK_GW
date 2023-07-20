611f68f91974c88bff13aee3a341b05f05528062
Park Ki Jeoung <parkyj477@gmail.com>
2023-07-20 14:56:06 +0900

    쉐이더 매니저 벡터 해제시 동적할당 해제

--- Dev/Enemy Slasher/Enemy Slasher/ShaderManager.cpp
+++ Dev/Enemy Slasher/Enemy Slasher/ShaderManager.cpp
@@ -8,6 +8,8 @@ CShaderManager::CShaderManager()
 
 CShaderManager::~CShaderManager()
 {
+	for (CShader* a : m_vShaderManager) delete a;
+	m_vShaderManager.clear();
 }
 
 void CShaderManager::BuildShaders(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, ID3D12RootSignature* m_pd3dGraphicsRootSignature)
@@ -27,5 +29,6 @@ void CShaderManager::BuildShaders(ID3D12Device* pd3dDevice, ID3D12GraphicsComman
 
 void CShaderManager::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera, ShaderType pShaderType)
 {
+	// 퍼포먼스적 문제가 있을 경우 쓸데없는 쉐이더 등록을 많이 하고 있을 수 있음
 	m_vShaderManager[(int)pShaderType]->Render(pd3dCommandList, pCamera);
 }
--- Dev/Enemy Slasher/Enemy Slasher/ShaderManager.h
+++ Dev/Enemy Slasher/Enemy Slasher/ShaderManager.h
@@ -7,7 +7,7 @@ class CShaderManager
 	// 일단 쉐이더 매니저는 모든 쉐이더를 가지고 있는 것으로 개발
 private:
 	std::vector<CShader*> m_vShaderManager;
-	
+	ShaderType m_UsingShader;
 
 public:
 	CShaderManager();
