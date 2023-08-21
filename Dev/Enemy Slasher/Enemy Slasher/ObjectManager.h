9f88e0f61c27b3b5ab07a0166dfbdff5a7701674
Park Ki Jeoung <parkyj477@gmail.com>
2023-08-22 07:29:35 +0900

    Terrain이 나중에 출력되어 Object Layer의 Object들이 출력되지 않는 오류 - Terrain을 먼저 출력하도록 변경

--- Dev/Enemy Slasher/Enemy Slasher/ObjectManager.h
+++ Dev/Enemy Slasher/Enemy Slasher/ObjectManager.h
@@ -6,6 +6,7 @@ enum class ObjectLayer : int;
 
 enum class ObjectLayer : int { // enum class는 int형으로 암시적 변환을 불허함으로 명시적 형변환을 해야 함
 	BackGround,				// 배경 오브젝트
+	Terrain,				// 터레인 오브젝트
 	LightObject,			// 조명 오브젝트
 	InteractiveObject,		// 상호작용가능 오브젝트 (ex 문, 버튼)
 	Object,					// 중립 오브젝트
@@ -14,7 +15,6 @@ enum class ObjectLayer : int { // enum class는 int형으로 암시적 변환을 불허함으�
 	Enemy,					// 적 오브젝트
 	Player,					// 플레이어 오브젝트
 	UIObject,				// 플레이어 UI 오브젝트	 depth/stancil 무시하고 항상 앞에 그려져야 함
-	Terrain,				// 터레인 오브젝트
 	Count					// Layer 개수
 };
 
