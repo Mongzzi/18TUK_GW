480847509d5515d29d80344e055155eb8e6e478c
Park Ki Jeoung <parkyj477@gmail.com>
2023-08-21 19:47:14 +0900

    조명을 위해 임시 VS,PS 작성 및 CObjectNormalShader 작동 테스트

--- Dev/Enemy Slasher/Enemy Slasher/ShaderManager.cpp
+++ Dev/Enemy Slasher/Enemy Slasher/ShaderManager.cpp
@@ -31,10 +31,10 @@ void CShaderManager::BuildShaders(ID3D12Device* pd3dDevice, ID3D12GraphicsComman
 	shader->CreateShader(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature);
 	m_vShaderManager.push_back(shader);
 
-	//// CObjectNormalShader
-	//shader = new CObjectsNormalShader();
-	//shader->CreateShader(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature);
-	//m_vShaderManager.push_back(shader);
+	// CObjectNormalShader
+	shader = new CObjectsNormalShader();
+	shader->CreateShader(pd3dDevice, pd3dCommandList, m_pd3dGraphicsRootSignature);
+	m_vShaderManager.push_back(shader);
 
 }
 
--- Dev/Enemy Slasher/Enemy Slasher/Shaders.hlsl
+++ Dev/Enemy Slasher/Enemy Slasher/Shaders.hlsl
@@ -55,3 +55,26 @@ float4 PSStandard(VS_STANDARD_OUTPUT input) : SV_TARGET
 
 
 
+VS_STANDARD_OUTPUT VSLighting(VS_STANDARD_INPUT input)
+{
+    VS_STANDARD_OUTPUT output;
+
+    //output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+    output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+    output.color = input.color;
+
+    return (output);
+}
+
+float4 PSLighting(VS_STANDARD_OUTPUT input) : SV_TARGET
+{
+    //float4 cAlbedoColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    //float4 cSpecularColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    //float4 cEmissionColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+
+    //float4 cColor = cAlbedoColor + cSpecularColor + cEmissionColor;
+
+    //cColor = float4(1.0f, 0.0f, 0.0f, 1.0f);
+
+    return (input.color);
+}
