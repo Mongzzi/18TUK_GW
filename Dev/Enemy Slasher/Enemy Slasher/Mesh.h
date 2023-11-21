744aff873ed7f8408f3c18722dbf004734249d7c
Park Ki Jeoung <parkyj477@gmail.com>
2023-11-22 07:16:17 +0900

    헤더 상호참조 버그 수정
    권장하는 방향은 아님 - 주의

--- Dev/Enemy Slasher/Enemy Slasher/Mesh.h
+++ Dev/Enemy Slasher/Enemy Slasher/Mesh.h
@@ -6,6 +6,7 @@
 #include "Ray.h"
 #include "Skeleton.h"
 
+class CSkeleton;
 
 class CMesh
 {
--- Dev/Enemy Slasher/Enemy Slasher/Skeleton.h
+++ Dev/Enemy Slasher/Enemy Slasher/Skeleton.h
@@ -3,6 +3,8 @@
 #include "Mesh.h"
 #include "FbxLoader.h"
 
+class CFBXMesh;
+
 class CSkeleton
 {
 public:
